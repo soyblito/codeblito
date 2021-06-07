bdec7b5d150949ffbb9d9be44a1a834b0e9df490
developerBBV <developer@blackbox-vision.tech>
2021-06-07 08:49:25 -0300

    changes

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,9 +1,19 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
+
 const Button = () => {
+  const container = {
+    width: "100%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:'red',
+  };
+
   return (
-    <View>
+    <View style={container}>
       <Text>Soy un Boton</Text>
     </View>
   )
